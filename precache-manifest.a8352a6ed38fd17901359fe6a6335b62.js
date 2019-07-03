c8e1e73fdfea08d88f4d8d81185377bdde14a42e
Erickowski <daniel_perez_03081995@hotmail.com>
2019-07-03 14:30:34 -0500

    Primer commit

---
+++ precache-manifest.a8352a6ed38fd17901359fe6a6335b62.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b396ccd218c2dabb750a01c26f80d97d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee97d084233ae689446a",
+    "url": "/static/js/2.7c8a10b4.chunk.js"
+  },
+  {
+    "revision": "8211321f09d5ed1cc8c0",
+    "url": "/static/js/main.c18c5e5e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
